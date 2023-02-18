834670707f97b05ae95a5d5c4ba3b590846ab220
Suraj <surajbahadur58@gmail.com>
2023-02-18 12:11:06 +0530

    Schema Definition using Prishma

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = express()
 // write code here 
 app.get('/', (req, res) => {
 
-    console.log("Express Server")
+    console.log("Express Server is Running on port 3001")
     res.status(200)
     res.json( {
         message : "Server Running"
