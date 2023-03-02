8be29da01621dd81a2bf1b6ba80b4e58a7b172fe
Suraj <surajbahadur58@gmail.com>
2023-03-02 22:03:54 +0530

    update backend api created, need to fix modifyUpdate function

--- src/handlers/updateHandler.ts
+++ src/handlers/updateHandler.ts
@@ -2,118 +2,127 @@ import { body } from 'express-validator';
 import { createJWT } from './../modules/auth';
 import prisma from "../db"
 
-export const getUpdates = async(req, res) => {
+export const getUpdates = async (req, res) => {
 
 
-    const prod = await prisma.product.findMany( {
-        where : {
+    const prod = await prisma.product.findMany({
+        where: {
             belongsToID: req.user.id
         },
         include: {
-            updates : true
+            updates: true
         }
     })
-    
 
-    const updates = prod.reduce((allup , prod) => {
+
+    const updates = prod.reduce((allup, prod) => {
         return [...allup, ...prod.updates]
     }, [])
-    res.json({ data : updates })
-    
+    res.json({ data: updates })
+
 }
 
-export const getSingleUpdates = async(req, res) => {
-const singleUpdate = await prisma.update.findUnique({
-    where : {
-        id: req.params.id
-    }
-})
-
-res.json({data : singleUpdate})
-}
-
-export const addUpdates = async(req, res) => {
-    const product = await prisma.product.findUnique({
-        where : {
-            id : req.body.id
+export const getSingleUpdates = async (req, res) => {
+    const singleUpdate = await prisma.update.findUnique({
+        where: {
+            id: req.params.id
         }
     })
 
-    if (product) {
-        res.json({message : "not valid"})
+    res.json({ data: singleUpdate })
+}
+
+export const addUpdates = async (req, res) => {
+
+
+    // const {productId, ...rest} = req.body
+    const product = await prisma.product.findUnique({
+        where: {
+            id: req.body.id
+        }
+    })
+
+    if (!product) {
+        res.json({ message: "not valid" })
     }
 
     const update = await prisma.update.create({
-        data : req.body
-    })
-
-
-    res.json( { data : update})
-}
-
-export const modifyUpdates = async(req, res) => {
-    const products = await prisma.product.findMany({
-        where : {
-            belongsToID : req.user.id,
-            
-        },
-        include : {
-            updates : true
+        data: {
+            title: req.body.title,
+            body: req.body.body,
+            productto: { connect: { id: product.id } }
         }
     })
 
 
-    const updates = products.reduce((allup , prod) => {
+    res.json({ data: update })
+}
+
+export const modifyUpdates = async (req, res) => {
+    const products = await prisma.product.findMany({
+        where: {
+            belongsToID: req.user.id,
+
+        },
+        include: {
+            updates: true
+        }
+    })
+
+    // console.log(products)
+
+
+    const updates = products.reduce((allup, prod) => {
         return [...allup, ...prod.updates]
     }, [])
 
 
-    const match = updates.find( update => update.id === req.params.id)
+    const match = updates.find(update => update.id === req.params.id)
 
     if (!match) {
-        res.json({ message : 'nope'})
+        return res.json({ message: 'nope' })
 
     }
     const uploadUpdate = await prisma.update.update({
-        where : {
-            id : req.params.id
+        where: {
+            id: req.params.id
         },
 
-        data : req.body
+        data: req.body
     })
 
-    res.json( { data : uploadUpdate})
+    res.json({ data: uploadUpdate })
 }
 
-export const deleteUpdates = async(req, res) => {
+export const deleteUpdates = async (req, res) => {
     const products = await prisma.product.findMany({
-        where : {
-            belongsToID : req.user.id,
-            
+        where: {
+            belongsToID: req.user.id,
+
         },
-        include : {
-            updates : true
+        include: {
+            updates: true
         }
     })
 
 
-    const updates = products.reduce((allup , prod) => {
+    const updates = products.reduce((allup, prod) => {
         return [...allup, ...prod.updates]
     }, [])
 
 
-    const match = updates.find( update => update.id === req.params.id)
+    const match = updates.find(update => update.id === req.params.id)
 
     if (!match) {
-        res.json({ message : 'nope'})
+        res.json({ message: 'nope' })
 
     }
-    
-    const deleted = await prisma.update.delete( {
-        where : {
-           id : req.params.id  
+
+    const deleted = await prisma.update.delete({
+        where: {
+            id: req.params.id
         }
     })
 
-    res.join({ data : deleted})
+    res.json({ data: deleted })
 }
--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,4 @@
+import { addUpdates, deleteUpdates, modifyUpdates, getUpdates, getSingleUpdates } from './handlers/updateHandler';
 import { Router } from 'express'
 import { body } from 'express-validator'
 import { createProduct, deleteProduct, getAProdcut, getProducts, updateProduct } from './handlers/productHandler'
@@ -15,28 +16,15 @@ router.delete('/product/:id', deleteProduct) // delete single product
 
 
 
-router.get('/updates', () => { }) //get updates details
-router.get('/update/:id', () => { }) // single update details
+router.get('/updates', getUpdates) //get updates details
+router.get('/update/:id', getSingleUpdates) // single update details
+router.post('/update', body('title').exists().isString(), body('body').exists().isString(), body('productId').exists().isString(), addUpdates) // create update
+router.put('/update/:id', body('title').optional, body('body').optional, body('version').optional, body('status').isIn(['IN_PROGRESS', 'LIVE', 'DEPRECATED', 'ARCHIVED']).optional(), modifyUpdates) // update single update
+router.delete('/update/:id', deleteUpdates) // delete single update
 
-router.post('/update',
-    body('title').exists().isString(),
-    body('body').exists().isString(),
-    body('productId').exists().isString()
-    , () => { }) // create update
-router.put('/update/:id',
-
-    body('title').optional,
-    body('body').optional,
-    body('version').optional,
-    body('status').isIn(['IN_PROGRESS', 'LIVE', 'DEPRECATED', 'ARCHIVED']).optional()
-
-    , () => { }) // update single update
-router.delete('/update/:id', () => { }) // delete single update
 
 router.get('/updatepoints', () => { }) //get updates details
 router.get('/updatepoint/:id', () => { }) // single update details
-
-
 router.post('/updatepoint',
     body('name').isString,
     body('description').isString(),
